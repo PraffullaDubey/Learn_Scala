ff2ba876b3b402fa33c8c330d8e23bea3b6964de
Praffulla Kumar Dubey <praffullakrdubey@gmail.com>
2022-11-20 22:00:23 +0530

    While and Do While Loops

---
+++ Project_1/src/WhileLoop.scala
@@ -0,0 +1,12 @@
+object WhileLoop {
+  def main(args: Array[String]): Unit ={
+    var x = 0;
+    while (x < 10) {
+      println("x = " + x);
+      x += 1; //x = x + 1
+      /*x++ x-- ++x --x -> these are not
+      allowed in Scala*/
+    }
+    }
+  }
+
